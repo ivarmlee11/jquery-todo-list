ca5b2ba8fa66acefd2c08823cff9e41596f328c4
Ivar Lee <ivarr5@gmail.com>
2016-06-01 01:05:15 -0700

    added comments

--- js/app.js
+++ js/app.js
@@ -1,7 +1,10 @@
 
 $(document).ready(function() {
 
-
+//the value of the text box is appanded to the list
+//in a concat'd html string
+//item is also sent to local storage with the .setItem method
+//takes two params, object key name and value name
 
 $('#toDoListButton').on('click', function(e){
   e.preventDefault();
@@ -12,6 +15,18 @@ $('#toDoListButton').on('click', function(e){
 });
 
 
+// for in loop grabs properties in the localStorage Object
+// .getItem(property) returns a string which is the corresping key
+for(var i in window.localStorage){
+  console.log(localStorage)
+  console.log(val);
+$('#sortable').append('<li class="listStuff">'+val+'</li>');
+};
+
+
+//this button calls the .clear method on local storage
+//and fills the unordered list element with a blank html string "" to clear it
+//mouse cursor goes back to the text input
 $('#lsRemoveButton').on('click', function(e){
   e.preventDefault();
   localStorage.clear();
@@ -20,21 +35,17 @@ $('#lsRemoveButton').on('click', function(e){
 });
 
 
+// this is the list with the UI plugin
+// this is event delegtion, events are delegated
+// to newly made list items
+// when they get clicked they'll fade out over 5 seconds
+// focus (mouse cursor) goes back to the text input
 $('#sortable').on('click', 'li', function(){
   $(this).fadeOut(5000);
   $('#textArea').focus();
 });
 
+// this is the UI plugin function .sortable
 $( "#sortable" ).sortable();
-
-for(var i in window.localStorage){
-  val = localStorage.getItem(i);
-  value = val.split(',');
-$('#sortable').append('<li class="listStuff">'+value+'</li>');
-}
-
-
-
-
 
 });
