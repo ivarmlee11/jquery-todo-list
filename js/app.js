3bdbaf9fa81bcb9328e8c1ba5266df9b7e3f38b9
Ivar Lee <ivarr5@gmail.com>
2016-06-01 07:38:02 -0700

    added comments

--- js/app.js
+++ js/app.js
@@ -1,4 +1,9 @@
 $(document).ready(function() {
+
+  //clicking on the add to list button stores the text
+  //value in a variable and appends it to the sortable list
+  //local storage is also updated with the setItem() method
+  //focus is returned to the text box
   $('#toDoListButton').on('click', function(e) {
     e.preventDefault();
     var temp = $('#textArea').val();
@@ -7,11 +12,16 @@ $(document).ready(function() {
     $('#textArea').val('').focus();
   });
 
+  //this runs on page load and gets local storage items by
+  //index number and adds them to the sortable list
   for (var i in window.localStorage) {
     var val = localStorage.getItem(i);
     $('#sortable').append('<li class="listStuff">' + val + '</li>');
   }
 
+
+  //list items are removed with this button click
+  //local storage is cleared as well
   $('#lsRemoveButton').on('click', function(e) {
     e.preventDefault();
     localStorage.clear();
@@ -19,11 +29,15 @@ $(document).ready(function() {
     $('#textArea').val('').focus();
   });
 
-
+  //event delegation, dynamically created list items
+  //fade out on click
+  //focus is returned to the text box
   $('#sortable').on('click', 'li', function() {
     $(this).fadeOut(5000);
     $('#textArea').focus();
   });
 
+
+  //this sortable method came with the jQuery plugin
   $('#sortable').sortable();
 });
