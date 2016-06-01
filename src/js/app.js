4be66c1a94272058901ce6a9b05eac090d71cfd6
Ivar Lee <ivarr5@gmail.com>
2016-06-01 01:20:22 -0700

    linted

--- src/js/app.js
+++ src/js/app.js
@@ -1,35 +1,29 @@
-
 $(document).ready(function() {
+  $('#toDoListButton').on('click', function(e) {
+    e.preventDefault();
+    var temp = $('#textArea').val();
+    $('#sortable').append('<li class="listStuff">' + temp + '</li>');
+    localStorage.setItem('item_' + temp, temp);
+    $('#textArea').val('').focus();
+  });
+
+  for (var i in window.localStorage) {
+    var val = localStorage.getItem(i);
+    $('#sortable').append('<li class="listStuff">' + val + '</li>');
+  }
+
+  $('#lsRemoveButton').on('click', function(e) {
+    e.preventDefault();
+    localStorage.clear();
+    $('#sortable').html('');
+    $('#textArea').val('').focus();
+  });
 
 
+  $('#sortable').on('click', 'li', function() {
+    $(this).fadeOut(5000);
+    $('#textArea').focus();
+  });
 
-$('#toDoListButton').on('click', function(e){
-  e.preventDefault();
-  var temp = $('#textArea').val();
-  $('#sortable').append("<li class='listStuff'>" + temp + "</li>");
-  localStorage.setItem('item_'+temp,temp);;
-  $('#textArea').val('').focus();
-});
-
-for(var i in window.localStorage){
-  val = localStorage.getItem(i);
-  console.log(val);
-$('#sortable').append('<li class="listStuff">'+val+'</li>');
-}
-
-$('#lsRemoveButton').on('click', function(e){
-  e.preventDefault();
-  localStorage.clear();
-  $("#sortable").html('');
-  $('#textArea').val('').focus();
-});
-
-
-$('#sortable').on('click', 'li', function(){
-  $(this).fadeOut(5000);
-  $('#textArea').focus();
-});
-
-$( "#sortable" ).sortable();
-
+  $('#sortable').sortable();
 });
